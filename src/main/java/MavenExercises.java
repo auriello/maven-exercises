5d5c998ff274e2bada6d491db022769bc9ee634b
auriel grant <aurgrant@gmail.com>
2023-05-05 09:19:38 -0500

    maven exercises

---
+++ src/main/java/MavenExercises.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+public class MavenExercises {
+    public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter Something: ");
+                String input = scanner.nextLine();
+
+                System.out.println("You Entered: \"" + input + "\"");
+
+                if (StringUtils.isNumeric(input)) {
+                    System.out.println("\"" + input + "\" is a number");
+                } else {
+                    System.out.println("\"" + input + "\" is not a number");
+
+                    String flipped = StringUtils.swapCase(input);
+                    System.out.println("Flipped Case: " + flipped);
+
+                    String reversed = StringUtils.reverse(input);
+                    System.out.println("Reversed: " + reversed);
+                }
+            }
+
+        }
+
